b4e3dde1fb9a21ec052251a12843e03c9fcf936f
Jack-170 <agavejack@hotmail>
2024-01-08 17:04:08 +0100

    feat: add header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,75 @@
+<script>
+
+export default{
+    name:'Appheader',
+
+    props:['message'],
+
+    methods:{
+        GetImagePath: function(imgPath){
+            return new URL(imgPath, import.meta.url ).href;
+        }
+    }
+}
+
+</script>
+
+<template>
+
+
+<header>
+
+    <div class="container-fluid">
+
+        <div class="navbar d-flex justify-content-start">
+    
+           <div class="ms-box-img">
+            <img src="../assets/logo.png" alt="">
+           </div>
+    
+           <h2>{{ message }}</h2>
+    
+        </div>
+    
+     </div>
+    
+</header>
+ 
+  
+</template>
+
+<style lang="scss" scoped>
+
+@use '../styles/general.scss' as *;
+
+
+
+header{
+    height: 90px;
+
+    .container-fluid{
+        height: 100%;
+    
+        .navbar{
+            height: 70px;
+            margin: 10px auto;
+    
+            .ms-box-img{
+                height: 40px;
+                width: 100px;
+                object-fit: contain;
+    
+                img{
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+</style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,37 @@
+<script>
+
+export default{
+    name:'CardList',
+
+
+}
+
+</script>
+
+<template>
+
+ <div class="container-fluid">
+
+    <div class="container"></div>
+
+ </div>
+  
+</template>
+
+<style lang="scss" scoped>
+
+@use '../styles/general.scss' as *;
+
+.container-fluid{
+    height: 800px;
+    background-color: #d48f38;
+
+    .container{
+        background-color: white;
+        height:  600px;
+    }
+}
+
+
+
+</style>
